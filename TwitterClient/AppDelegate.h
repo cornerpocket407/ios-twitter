85f8aa1404711cc7a3e40435c14488ed08833840
Tony Dao <tony.dao@Admins-MacBook-Pro.local>
2014-03-28 21:51:39 -0700

    Initial Commit

---
+++ TwitterClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterClient
+//
+//  Created by Tony Dao on 3/28/14.
+//  Copyright (c) 2014 Tony Dao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
